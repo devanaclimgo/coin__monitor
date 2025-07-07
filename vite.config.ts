20d344e345d8ab67c261c00200886bbab8571cc2
devanaclimgo <anaclimgo@gmail.com>
2025-07-07 09:43:32 -0300

    build: added vite ts configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    react(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './app/frontend'),
+      crypto: 'crypto-browserify'
+    },
+  },
+  optimizeDeps: {
+    include: [
+      '@react-three/fiber',
+      '@react-three/drei',
+      'three',
+      'crypto-browserify'
+    ],
+  },
 })
