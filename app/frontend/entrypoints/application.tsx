c882fa706bf47bb4534be83d550bca28ccab1bdd
devanaclimgo <anaclimgo@gmail.com>
2025-07-07 10:21:52 -0300

    feat: application tsx root added

---
+++ app/frontend/entrypoints/application.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CurrencyDashboard from '../components/CurrencyDashboard';
+import '@hotwired/turbo-rails';
+import 'chart.js/auto';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <CurrencyDashboard />
+      </React.StrictMode>
+    );
+  }
+});
