21d8e402c4229d9f83f54e9d9193e942d241a2bf
devanaclimgo <anaclimgo@gmail.com>
2025-07-07 11:09:26 -0300

    feat: added home controller currency and index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,30 @@
+require "net/http"
+require "json"
+
 class HomeController < ApplicationController
+  CURRENCIES = [
+    { code: "USD-BRL" },
+    { code: "EUR-BRL" },
+    { code: "BTC-BRL" }
+  ]
+
+  def index
+    @chart_data = []
+
+    CURRENCIES.each do |currency|
+      url = URI("https://economia.awesomeapi.com.br/json/daily/#{currency[:code]}/15")
+      response = Net::HTTP.get(url)
+      data = JSON.parse(response)
+
+      {
+      name: currency[:code],
+      data: data.each_with_object({}) do |entry, hash|
+        date = Time.at(entry["timestamp"].to_i).strftime("%d/%m/%y")
+        hash[date] = entry["high"].to_f
+      end
+    }
+    end
+
+    puts @chart_data.inspect
+  end
 end
