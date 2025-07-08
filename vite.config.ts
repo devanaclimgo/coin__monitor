8a79ee3a2eb60b451a413b99d0a969eb12dd2c83
devanaclimgo <anaclimgo@gmail.com>
2025-07-08 08:52:05 -0300

    feat: added threejs path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app/frontend'),
+      'three': path.resolve(__dirname, './node_modules/three'),
       crypto: 'crypto-browserify'
     },
   },
