3355f81a830305c7d3b2d459e9dda46453932647
devanaclimgo <anaclimgo@gmail.com>
2025-07-08 09:10:56 -0300

    feat: added type definitions coin and currency data

--- app/frontend/types/index.ts
+++ app/frontend/types/index.ts
@@ -1,8 +1,19 @@
 export interface CurrencyData {
-  name: string;
-  symbol: string;
-  price: number;
-  change24h: number;
-  sparklineData: number[];
-  color: string;
+  symbol: string
+  name: string
+  price: number
+  change24h: number
+  sparklineData: number[]
+  color: string
+}
+
+export interface Coin {
+  id: string
+  name: string
+  symbol: string
+  current_price: number
+  price_change_percentage_24h: number
+  market_cap: number
+  volume_24h: number
+  image: string
 }
