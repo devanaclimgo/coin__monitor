a7b27adf29c8b37ab5bd6c25d54c8188adcfae77
devanaclimgo <anaclimgo@gmail.com>
2025-07-08 18:27:59 -0300

    build: added route to application frontend path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "*path", to: "home#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "application#frontend", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
