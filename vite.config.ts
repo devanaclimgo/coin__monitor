5d7e30730477e5ad75338bf5b1ac92653761d3de
devanaclimgo <anaclimgo@gmail.com>
2025-07-10 12:09:36 -0300

    build: updated proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,29 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     react({
       babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { legacy: true }]
-        ]
-      }
+        plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+      },
     }),
   ],
   build: {
     outDir: '../public/vite-assets',
     emptyOutDir: true,
-    manifest: true
+    manifest: true,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
       '@': path.resolve(__dirname, './'),
-      'three': path.resolve(__dirname, './node_modules/three'),
-      crypto: 'crypto-browserify'
+      three: path.resolve(__dirname, './node_modules/three'),
+      crypto: 'crypto-browserify',
     },
   },
   optimizeDeps: {
@@ -35,21 +33,22 @@ export default defineConfig({
       'three',
       'crypto-browserify',
       'chartkick',
-      'chart.js'
+      'chart.js',
     ],
   },
   css: {
     postcss: {
-     plugins: [
-        autoprefixer()
-      ],
+      plugins: [autoprefixer()],
     },
   },
-   server: {
+  server: {
     port: 3036,
+    strictPort: true,
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/assets': 'http://localhost:3000'
-    }
+      '^/(assets|api|vite-dev)': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
-})
+});
