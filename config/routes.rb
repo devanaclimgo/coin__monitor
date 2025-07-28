27632dfce205b35b6227c6af669a9c64ca36bd05
devanaclimgo <anaclimgo@gmail.com>
2025-07-28 15:20:01 -0300

    feat: added api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api do
+    get "currency_data", to: "currency#index"
+  end
+
   root "home#index"
 end
