609e1af3f24cab402bf13a60f34cffdd6d8bc502
devanaclimgo <anaclimgo@gmail.com>
2025-07-28 13:27:16 -0300

    feat: created app file

---
+++ app/frontend/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import CryptoMonitor3D from './CryptoMonitor3D'
+
+export default function App() {
+  return (
+    <Canvas>
+      <CryptoMonitor3D />
+    </Canvas>
+  )
+}
