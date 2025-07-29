c3dc6b7c5bc249b3c26761e45cfe15bba0697990
devanaclimgo <anaclimgo@gmail.com>
2025-07-29 09:23:54 -0300

    fix: deleted unnecessary canvas

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
 import CryptoMonitor3D from './CryptoMonitor3D'
 
 export default function App() {
   return (
-    <Canvas>
       <CryptoMonitor3D />
-    </Canvas>
   )
 }
