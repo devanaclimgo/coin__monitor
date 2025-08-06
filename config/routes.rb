3d557d787691e86ade5b35b30a526e60630361c5
devanaclimgo <anaclimgo@gmail.com>
2025-08-06 19:15:35 -0300

    fix: edited get to only currency

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   namespace :api do
-    get "currency_data", to: "currency#index"
+    get "currency", to: "currency#index"
   end
 
   root "home#index"
