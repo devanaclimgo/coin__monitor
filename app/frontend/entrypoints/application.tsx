c46c445d0dc23e55882e2f5719faba60321d6ecd
devanaclimgo <anaclimgo@gmail.com>
2025-08-07 10:55:50 -0300

    chore: edited css import to the top

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,6 +1,6 @@
+import '../styles/application.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../styles/application.css'
 import App from '../components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
