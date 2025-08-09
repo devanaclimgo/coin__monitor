d03616d4baef9d2aaeaf539b56f8523b734e128d
devanaclimgo <anaclimgo@gmail.com>
2025-08-09 16:51:22 -0300

    fix: edited class name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 require "net/http"
 require "json"
 
-class CurrencyController < ApplicationController
+class HomeController < ApplicationController
   CURRENCIES = [
     { code: "USD-BRL" },
     { code: "EUR-BRL" },
