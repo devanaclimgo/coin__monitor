daab4a130e2884d7db3f1b652eb0f4b921bd24a0
devanaclimgo <anaclimgo@gmail.com>
2025-08-10 15:35:09 -0300

    chore: edited html erb view directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   darkMode: ['class'],
   content: [
     './app/frontend/**/*.{js,ts,jsx,tsx}',
-    './app/views/**/*.html.erb',
+    './app/views/**/*.{html,erb}',
     './app/helpers/**/*.rb',
   ],
   theme: {
