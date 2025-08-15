7a7b6aaa88a9c3679b3dc07ef9d3e6933dc0bc79
devanaclimgo <anaclimgo@gmail.com>
2025-08-14 21:09:18 -0300

    chore: edited root to mount on cryptomonitor

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '../styles/application.css'
-import App from '../components/App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "../styles/application.css"
+import CryptoMonitor from "../components/CryptoMonitor"
 
-const el = document.getElementById('root')!
-const root = ReactDOM.createRoot(el)
-root.render(<App />)
+const rootElement = document.getElementById("root")
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <CryptoMonitor />
+    </React.StrictMode>
+  )
+} else {
+  console.error("❌ Could not find root element to mount React app")
+}
