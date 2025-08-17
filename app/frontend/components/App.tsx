11e4861687b278440dcc33bda57106ad935dc85c
devanaclimgo <anaclimgo@gmail.com>
2025-08-16 21:02:55 -0300

    refactor: frontend folder deleted

--- app/frontend/components/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import CryptoMonitor from './CryptoMonitor'
-
-export default function App() {
-  return <CryptoMonitor />
-}
--- app/frontend/types/chartkick.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'chartkick' {
-  export function useChart(): any; 
-  export const ColumnChart: React.ComponentType<{
-    data: Record<string, number> | [string, number][];
-    xtitle?: string;
-    ytitle?: string;
-    height?: string
-  }>;
-}
